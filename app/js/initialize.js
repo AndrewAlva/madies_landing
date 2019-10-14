077fa17df11a9ab50c510028e581b0f9325ab5a9
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2019-10-13 23:13:21 -0500

    Mobile MVP: Slider animation added, spanish version created.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -3,7 +3,10 @@
 // without waiting for stylesheets, images, and
 // subframes to finish loading
 document.addEventListener('DOMContentLoaded', function() {
-    // Do something
+	// Activate scrollTop
+    document.getElementById('backTop').addEventListener('click', function(){
+    	toTop();
+    });
 });
 
 
@@ -12,4 +15,10 @@ window.onload = function() {
     // Do something, remove preloader perhaps
     console.log("Page fully loaded.");
     console.log("Initialize.js");
+
+    // Animiation frame loop at 60fps to enable "toTop()" function
+    RAF.init();
+
+    var FruitsSlider = new Carousel('fruits-slider');
+    FruitsSlider.init();
 }
