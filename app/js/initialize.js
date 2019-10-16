0c27471797a7fe160b2c61288fe26ce0d6683789
Kenny <mac@MacBook-Pro-de-mac.local>
2019-10-15 21:10:10 -0700

    Added humans for deploy.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', function() {
 // Trigger functions after page is completely loaded
 window.onload = function() {
     // Do something, remove preloader perhaps
-    console.log("Page fully loaded.");
-    console.log("Initialize.js");
+    // console.log("Page fully loaded.");
+    // console.log("Initialize.js");
 
     // Animiation frame loop at 60fps to enable "toTop()" function
     RAF.init();
@@ -29,4 +29,18 @@ window.onload = function() {
     // Enable slider functionality
     var FruitsSlider = new Carousel('fruits-slider');
     FruitsSlider.init();
+
+    // Humans Credits
+    console.log("Site designed and developed at Mandelbrot Studio®");
+    console.log("https://mandelbrot.mx/");
+    console.log("");
+    console.log("UI Design: Zero Morales");
+    console.log("https://www.instagram.com/zero_alex/");
+    console.log("");
+    console.log("Front-End Development:");
+    console.log("Kenny Celis");
+    console.log("https://www.instagram.com/hey_mr_kenny/");
+    console.log("");
+    console.log("Andrew Alvarado");
+    console.log("https://www.instagram.com/andrew__alva/");
 }
