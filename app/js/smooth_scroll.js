9df2c90ab0a7aaca60bb85337486683bc8209a66
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2019-11-28 15:04:03 -0600

    Smooth scroll optimized to lite version, need less JS, CSS and HTML; only implemented on EN mob and desk versions Firefox layout issues still pending.

--- app/js/smooth_scroll.js
+++ app/js/smooth_scroll.js
@@ -5,121 +5,53 @@ var SmoothScroll = function(args){
 	
 	this.ui = {
 		el: document.querySelector('.js-scroll'),
-		sections: document.querySelectorAll('.js-scroll-section'),
 		heightEl: null      
 	}
 	
 	this.state = {
-		total: this.ui.sections.length,
 		scroll: {
 			target: 0,
 			current: 0,
 			ease: 0.095
 		},
 		bounds: {
-			height: window.innerHeight,
-			scrollHeight: 0,
-			threshold: 400
+			scrollHeight: 0
 		},
 		isResizing: false
 	}
    
-	this.sections = null;
 
 	this.init = function(){
 		this.bindAll();
 		this.setInitial();
-		this.getThreshold();
 		this.setFakeHeight();
-		this.getSections();
 		this.addListeners();
 	}
 
 	this.bindAll = function(){
-		var _this = this; // "_this" could be replaced by _self, maybe
 		['onScroll', 'onResize', 'render'].forEach(function (fn) {
-			return _this[fn] = _this[fn].bind(_this);
+			return _self[fn] = _self[fn].bind(_self);
 		});
 	}
 
-	this.render = function(){ // "render" could be renamed to "render" to use with RAF
+	this.render = function(){
 		var scroll = this.state.scroll;
 
 
 		scroll.current += (scroll.target - scroll.current) * scroll.ease;
 		if (scroll.current < .1) scroll.current = 0;
 
-		this.transformSections();
+		this.translateContainer();
 	}
 
-	this.transformSections = function(){
+	this.translateContainer = function() {
 		var _state = this.state;
-		var total = _state.total;
 		var isResizing = _state.isResizing;
 		var scroll = _state.scroll;
 
 		var translate = "translate3d(0, " + -scroll.current + "px, 0)";
 
-		for (var i = 0; i < total; i++) {
-			var data = _self.sections[i];
-			var el = data.el;
-			var bounds = data.bounds;
-
-			var isVisible = _self.isVisible(bounds);
-
-			if (isVisible || isResizing) {
-				Object.assign(data, { out: false });
-				el.style.transform = translate;
-			} else if (!data.out) {
-				Object.assign(data, { out: true });
-				el.style.transform = translate;
-			}
-		}
-	}
-
-	this.isVisible = function(bounds){
-		var _state$bounds = this.state.bounds;
-		var height = _state$bounds.height;
-		var threshold = _state$bounds.threshold;
-
-		var current = this.state.scroll.current;
-
-		var top = bounds.top;
-		var bottom = bounds.bottom;
-
-
-		var start = top - current;
-		var end = bottom - current;
-		var isVisible = start < threshold + height && end > -threshold;
-
-		return isVisible;
-	}
-
-	this.getSections = function(){
-		var _this = this;
-
-		if (!this.ui.sections) return;
-		this.sections = [];
-
-		this.ui.sections.forEach(function (el) {
-			el.style.transform = 'translate3d(0, 0, 0)';
-
-			var _el$getBoundingClient = el.getBoundingClientRect();
-
-			var top = _el$getBoundingClient.top;
-			var bottom = _el$getBoundingClient.bottom;
-
-			var state = {
-				el: el,
-				bounds: {
-					top: top,
-					bottom: bottom
-				},
-				out: true
-			};
-
-			_this.sections.push(state);
-		});
+		_self.ui.el.style.transform = translate;
 	}
 
 	this.setInitial = function(){
@@ -129,8 +61,7 @@ var SmoothScroll = function(args){
 			position: 'fixed',
 			top: 0,
 			left: 0,
-			width: '100%',
-			height: '100%'
+			width: '100%'
 		});
 
 		document.body.classList.add('is-smooth-scroll');
@@ -138,7 +69,6 @@ var SmoothScroll = function(args){
 
 	this.setFakeHeight = function(){
 		var _state = this.state;
-		var total = _state.total;
 		var bounds = _state.bounds;
 
 
@@ -147,28 +77,12 @@ var SmoothScroll = function(args){
 			this.ui.heightEl.classList.add('js-fake-scroll');
 			document.body.appendChild(this.ui.heightEl);
 		}
-		var _ui$sections$getBound = this.ui.sections[total - 1].getBoundingClientRect();
 
-		var bottom = _ui$sections$getBound.bottom;
+		var bottom = _self.ui.el.getBoundingClientRect().height;
 
 		bounds.scrollHeight = bottom;
 
 		this.ui.heightEl.style.height = bottom + 'px';
-	}
-
-	this.getThreshold = function(){
-		var _thresholdRatio;
-		if (deviceDetector.device == "mobile") {
-			// _thresholdRatio = 52.5;
-			_thresholdRatio = 60;
-			this.state.bounds.threshold = this.state.bounds.scrollHeight;
-			if (this.state.bounds.threshold == 0) this.state.bounds.threshold = (_thresholdRatio * window.innerWidth) / 100;
-		} else {
-			_thresholdRatio = 26.04;
-			this.state.bounds.threshold = (_thresholdRatio * window.innerWidth) / 100;
-		}
-
-		console.log(this.state.bounds.threshold)
 	}
 
 	this.onScroll = function(){
@@ -179,29 +93,8 @@ var SmoothScroll = function(args){
 	this.onResize = function(){
 		this.state.isResizing = true;
 
-		if (this.sections) {
-			this.sections.forEach(function (_ref) {
-				var el = _ref.el;
-				var bounds = _ref.bounds;
-
-				el.style.transform = 'translate3d(0, 0, 0)';
-
-				var _el$getBoundingClient = el.getBoundingClientRect();
-
-				var top = _el$getBoundingClient.top;
-				var bottom = _el$getBoundingClient.bottom;
-
-
-				bounds.top = top;
-				bounds.bottom = bottom;
-			});
-
-			// this.transformSections();
-		}
-
 		this.setFakeHeight();
-		this.getThreshold();
-		this.transformSections();
+		this.translateContainer();
 
 		this.state.isResizing = false;
 	}
